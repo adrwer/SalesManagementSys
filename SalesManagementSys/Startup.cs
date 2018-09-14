b10ad81fa22d2a7150c89b8fcc85a7007f34c41d
Adrian Werimo <adrwer7@gmail.com>
2018-09-14 13:38:08 +0300

    Add project files.

---
+++ SalesManagementSys/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalesManagementSys.Startup))]
+namespace SalesManagementSys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
